0cf495d9232e9c88116aa53a9184b97e04109ac9
Trévis Morvany <trevis.morvany@cea.fr>
2025-11-27 11:07:03 +0100

    Change the workspace symbols telescope binding

--- init.lua
+++ init.lua
@@ -602,7 +602,7 @@ require('lazy').setup({
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+          map('<leader>sW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
