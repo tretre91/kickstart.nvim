19b63ea50e8d8dfb2b30072db836ac965600178a
Trévis Morvany <trevismorvany@gmail.com>
2025-12-11 20:13:25 +0100

    Add surround bindings

--- init.lua
+++ init.lua
@@ -970,7 +970,19 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = '<leader>Sa', -- Add surrounding in Normal and Visual modes
+          delete = '<leader>Sd', -- Delete surrounding
+          find = '<leader>Sf', -- Find surrounding (to the right)
+          find_left = '<leader>SF', -- Find surrounding (to the left)
+          highlight = '<leader>Sh', -- Highlight surrounding
+          replace = '<leader>Sr', -- Replace surrounding
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
