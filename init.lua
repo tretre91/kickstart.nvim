3fc16a49472241f3a1716c518a560175368a8dc1
Trévis Morvany <trevismorvany@gmail.com>
2026-01-07 17:41:03 +0100

    Disable treesitter for diff and typst

--- init.lua
+++ init.lua
@@ -1014,6 +1014,7 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'diff', 'typst' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
